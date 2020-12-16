728894ea2200171ac6b06424822d261b5c12b034
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-12-16 01:38:07 -0300

    fix: removed macro extensions

--- src/buffer.h
+++ src/buffer.h
@@ -3,9 +3,8 @@
 
 #include <stddef.h>
 
-#define container_of(ptr, type, member) ({                      \
-             const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-             (type *)( (char *)__mptr - offsetof(type,member) );})
+#define container_of(ptr, type, member)                         \
+             (type *)( (char *)(ptr) - offsetof(type,member) );
 
 
 #define GROW_BUF_BY 100
